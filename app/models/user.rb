f1fb6238bc386902d44dc5f55bca2f0e7b599618
Maooooooooooo <72291924+Maooooooooooo@users.noreply.github.com>
2021-03-29 20:13:56 +0900

    新規登録の全角カナバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  with_options presence: true,format: { with: /\A[ァ-ヶー-]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :birth_day, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_03_28_073359) do
     t.string "last_name"
     t.string "first_name_kana"
     t.string "last_name_kana"
-    t.string "birth_day"
+    t.date "birth_day"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
