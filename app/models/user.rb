ff58f11e077e8d7c7915966f76dbd8e4d047ebbd
Maooooooooooo <72291924+Maooooooooooo@users.noreply.github.com>
2021-03-31 19:44:25 +0900

    単体テストコードの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
   validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   with_options presence: true,format: { with: /\A[ァ-ヶー-]+\z/ } do
     validates :first_name_kana
