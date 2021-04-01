cc346a261b24fc8c2a360af284358dfb18ebbf86
Maooooooooooo <72291924+Maooooooooooo@users.noreply.github.com>
2021-04-01 19:21:13 +0900

    user.rbのコメントの削除とネスト記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-    validates :first_name
-    validates :last_name
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+    validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー-]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
   end
-  validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-  with_options presence: true,format: { with: /\A[ァ-ヶー-]+\z/ } do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-  validates :birth_day, presence: true
-end
+end    
