986fff4abfa995ac4f903a32dcdedbec7fe56efa
Maooooooooooo <72291924+Maooooooooooo@users.noreply.github.com>
2021-04-20 20:28:49 +0900

    editのルート設定と編集画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create]
+  before_action :set_item, only:[:edit,:show,:update]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -24,12 +25,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:item_name,:explanation,:category_id,:condition_id,:delivery_fee_id,:ship_from_area_id,:prefecture_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
