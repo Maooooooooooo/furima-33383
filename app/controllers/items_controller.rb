4505d47b48a4da73e20327c426fcc6342a4858a3
Maooooooooooo <72291924+Maooooooooooo@users.noreply.github.com>
2021-05-03 17:32:49 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new,:create,:edit,:update]
+  before_action :authenticate_user!, only:[:new,:create,:edit,:update,:destroy]
   before_action :set_item, only:[:edit,:show,:update]
   before_action :item_root, only:[:edit,:update]
   before_action :purchase_root, only:[:edit,:update]
